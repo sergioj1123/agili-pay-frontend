6e0d70dcba86749e99a669584671e7552b460dda
Sérgio Félix Júnior <sergiofelix9@gmail.com>
2026-02-10 22:41:08 -0300

    chore: add default routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { MainLayout } from './layout/main-layout/main-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+      },
+      // Exemplo de Lazy Loading para Standalone:
+      // {
+      //   path: 'dashboard',
+      //   loadChildren: () =>
+      //     import('./modules/dashboard/dashboard.routes').then((m) => m.DASHBOARD_ROUTES),
+      // },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  },
+];
