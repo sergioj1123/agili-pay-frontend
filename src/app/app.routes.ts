8ee9f7e7e8ef987b140f79ef5f10b5e04aef4bb9
Sérgio Félix Júnior <sergiofelix9@gmail.com>
2026-02-18 21:51:35 -0300

    fix: update routing to redirect to login instead of dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,35 @@
 import { Routes } from '@angular/router';
 import { MainLayout } from './layout/main-layout/main-layout';
+import { Login } from './modules/auth/login/login';
 
 export const routes: Routes = [
   {
+    path: 'login',
+    component: Login,
+  },
+
+  {
     path: '',
     component: MainLayout,
+    //Use guard when already implemented
     children: [
-      {
-        path: '',
-        redirectTo: 'dashboard',
-        pathMatch: 'full',
-      },
-      // Exemplo de Lazy Loading para Standalone:
       // {
       //   path: 'dashboard',
-      //   loadChildren: () =>
-      //     import('./modules/dashboard/dashboard.routes').then((m) => m.DASHBOARD_ROUTES),
+      //   loadComponent: () =>
+      //     import('./modules/dashboard/dashboard.component').then((m) => m.DashboardComponent),
+      // },
+      // // Redireciona a raiz para o dashboard apenas se estiver dentro do MainLayout
+      // {
+      //   path: '',
+      //   redirectTo: 'dashboard',
+      //   pathMatch: 'full',
       // },
     ],
   },
+
+  // 3. Wildcard (Rota não encontrada)
   {
     path: '**',
-    redirectTo: 'dashboard',
+    redirectTo: 'login',
   },
 ];
