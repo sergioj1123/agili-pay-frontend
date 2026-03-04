d42008bb627553a00297690d1a74d41ae2db0767
Sérgio Félix Júnior <sergiofelix9@gmail.com>
2026-03-04 20:21:01 -0300

    feat: implement login functionality with form validation and error handling

--- src/app/modules/auth/login/login.ts
+++ src/app/modules/auth/login/login.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { InputField } from '../../../shared/components/input-field/input-field';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../../../core/services/authService';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -15,11 +17,38 @@ export class Login {
     password: new FormControl('', [Validators.required, Validators.minLength(8)]),
   });
 
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {}
+
   get emailControl() {
     return this.loginForm.get('email') as FormControl;
   }
 
   get passwordControl() {
     return this.loginForm.get('password') as FormControl;
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+
+      if (!email || !password) {
+        alert('Por favor, preencha todos os campos corretamente.');
+        return;
+      }
+
+      this.authService.login(email, password).subscribe({
+        next: () => {
+          this.router.navigate(['/dashboard']);
+        },
+        error: (error) => {
+          alert('Erro ao fazer login: ' + (error.error?.message || 'Tente novamente mais tarde.'));
+        },
+      });
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
   }
 }
