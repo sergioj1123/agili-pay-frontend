8093907de356407f325ce0dfb815acf8e947dacb
Sérgio Félix Júnior <sergiofelix9@gmail.com>
2026-03-04 19:59:33 -0300

    feat: replace form groups with input-field components in login form

--- src/app/modules/auth/login/login.ts
+++ src/app/modules/auth/login/login.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { InputField } from '../../../shared/components/input-field/input-field';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [InputField, CommonModule, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+  });
 
+  get emailControl() {
+    return this.loginForm.get('email') as FormControl;
+  }
+
+  get passwordControl() {
+    return this.loginForm.get('password') as FormControl;
+  }
 }
